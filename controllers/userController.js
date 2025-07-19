69f162b5a2f01e44b2aa2a67a2557eeb9e1fe187
Manaf-10 <manaf.alhujairi2006@gmail.com>
2025-07-19 19:39:10 +0300

    added the user ctrl

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,2 +1,27 @@
 const User = require('../models/User.js')
 const Movie = require('../models/Movie.js')
+
+
+const getUserById = async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id)
+    const movies = await Movie.find({author : user._id})
+    const data = {
+      _id: user._id,
+      first: user.first,
+      last: user.last,
+      picture: user.picture,
+      watched: user.watched,
+      watching: user.watching,
+      willWatch: user.willWatch
+    }
+
+    res.render("./users/profile.ejs",{user})
+  } catch (error){
+    console.error("An error has occurred finding a user!", error.message)
+  }
+}
+
+module.exports = {
+  getUserById
+}
