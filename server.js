af98270d448184721f854c287fd426c7acadda24
Manaf-10 <manaf.alhujairi2006@gmail.com>
2025-07-19 19:36:45 +0300

    added the server file with all the needed middle ware and depadancies (for now)

---
+++ server.js
@@ -0,0 +1,49 @@
+const express = require('express')
+const logger = require('morgan')
+const methodOverride = require('method-override')
+const session = require('express-session')
+const mongoose = require('./db')
+const authRouter = require('./routes/authRouter.js')
+const userRouter = require('./routes/userRouter.js')
+require('dotenv').config()
+
+const PORT = process.env.PORT || 3000
+const app = express()
+
+// Middleware
+app.use(logger('dev'))
+app.use(methodOverride('_method'))
+app.use(session({
+  secret: process.env.SESSION_SECRET,
+  resave: false,
+  saveUninitialized: true
+}))
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+app.use(express.static('public'))
+app.use('/uploads', express.static('uploads')) 
+
+// Set up local variables for views
+app.use((req, res, next) => {
+  res.locals.user = req.session.user
+  next()
+})
+
+// Routes
+app.use('/auth', authRouter)
+app.use('/users', userRouter)
+
+app.get('/', (req, res) => {
+  res.render('index.ejs', { user: req.session.user })
+})
+
+// Error Handling Middleware
+app.use((err, req, res, next) => {
+  console.error(err.stack)
+  res.status(500).send('Something broke!')
+})
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Running Server on Port ${PORT} . . . `)
+})
