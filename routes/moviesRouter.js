eadca30a0deed83c554f958d41f7617b748494e6
Abdulla-Zaid <abdulla4433@gmail.com>
2025-07-19 23:23:31 +0300

    some issue come

--- routes/moviesRouter.js
+++ routes/moviesRouter.js
@@ -5,4 +5,4 @@ const upload = require('../uploads/upload')
 
 // Home page - Display all movies and form to add new ones
 router.get('/', movieController.movie_index)
-router.post('/', upload.single('picture'), movieController.movie_create_post)
+// router.post('/', upload.single('picture'), movieController.movie_create_post)
