9dd503b7501defff66339822a5a00e91a0c4acae
Abdulla-Zaid <abdulla4433@gmail.com>
2025-07-19 23:10:49 +0300

    add movie index in controller

---
+++ controllers/movieController.js
@@ -0,0 +1,15 @@
+// controllers/movieController.js
+const Movie = require('../models/Movie')
+const User = require('../models/User')
+
+// Home page - Display all movies and allow adding to lists
+const movie_index = async (req, res) => {
+  try {
+    const movies = await Movie.find()
+    res.send("Home Page")
+    // res.render('movies/index', { movies, user: req.session.user })
+  } catch (error) {
+    console.error('Error fetching movies:', error)
+    res.status(500).send('Error fetching movies')
+  }
+}
