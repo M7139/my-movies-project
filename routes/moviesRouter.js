2a38c25106c2380e20620d2fbc9c0821e93f5160
M7139 <mohamedaljoamee@gmail.com>
2025-07-20 22:53:10 +0300

    added the router for the search function

---
+++ routes/moviesRouter.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const router = express.Router()
+const movieController = require('../controllers/movieController.js')
+
+// Home page - Display all movies and form to add new ones
+router.get('/', movieController.movieSearch_get)
+
+router.get('/movie-list', (req, res) => {
+  res.render('./movies/movie-list.ejs')
+})
+
+module.exports = router
