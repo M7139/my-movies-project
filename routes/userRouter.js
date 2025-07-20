244bc853d592509634fb8109a7b9d776fcdb7c49
Manaf-10 <manaf.alhujairi2006@gmail.com>
2025-07-20 10:53:20 +0300

    done the prifle edit ctrl,route,veiw (last thing to is delete the old picture from uploads file everytime the user edits his pfp)

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -1,8 +1,16 @@
 const express = require('express')
 const router = express.Router()
 const userController = require('../controllers/userController.js')
+const upload = require('../uploads/upload.js')
 
-
+router.get("/:id/edit", userController.user_edit_get)
+router.post('/:id', upload.single('picture'), userController.user_update_post)
 router.get('/:id', userController.getUserById)
+
+
+
+
+
+
 
 module.exports = router
