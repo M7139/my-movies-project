9d0aded05d0ace9032adf2f4ffd9626beada7dc4
Abdulla-Zaid <abdulla4433@gmail.com>
2025-07-21 12:31:14 +0300

    connect  Watching and Watched  and Will Watch to routes

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -7,6 +7,11 @@ router.get("/:id/edit", userController.user_edit_get)
 router.post('/:id', upload.single('picture'), userController.user_update_post)
 router.get('/:id', userController.user_profile_get)
 
+router.get('/:id/watching', userController.watching_list_get)
+router.get('/:id/watched', userController.watched_list_get)
+router.get('/:id/will-watch', userController.willWatch_list_get)
+router.post('/:id/remove/:movieId', userController.remove_from_list)
+
 
 
 
