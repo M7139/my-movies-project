689ad3b1469f7fca720366c23df0dd5c7f6e739d
Manaf-10 <manaf.alhujairi2006@gmail.com>
2025-07-24 01:35:20 +0300

    added actors section in the movie show and there styling

--- controllers/movieController.js
+++ controllers/movieController.js
@@ -35,17 +35,71 @@ const movieSearch_get = async (req, res) => {
 }
 
 
-
+// DEEP SEEK WAS USED TO IMPLEMENT THE WIKIPEDIA API SECION AND FOR STYLING TOO
 const movieDetail_get = async (req, res) => {
   const imdbID = req.params.imdbID
   const apiKey = process.env.OMDB_API_KEY
+  
   try {
+    // Fetch movie details from OMDB
     const response = await fetch(
       `http://www.omdbapi.com/?i=${imdbID}&apikey=${apiKey}`
     )
     const movie = await response.json()
+    
     if (movie.Response === 'True') {
-      res.render('./movies/show.ejs', { movie, user: req.session.user })
+      // Process actors to get their images from Wikipedia
+      const actors = movie.Actors ? movie.Actors.split(', ') : []
+      
+      // Create an array of promises to fetch actor images
+      const actorImagePromises = actors.map(async (actorName) => {
+        try {
+          // First, search Wikipedia for the actor's page
+          const searchResponse = await fetch(
+            `https://en.wikipedia.org/w/api.php?action=query&format=json&list=search&srsearch=${encodeURIComponent(actorName)}&origin=*`
+          )
+          const searchData = await searchResponse.json()
+          
+          if (searchData.query.search.length > 0) {
+            const pageTitle = searchData.query.search[0].title
+            
+            // Get page images
+            const imageResponse = await fetch(
+              `https://en.wikipedia.org/w/api.php?action=query&format=json&prop=pageimages&titles=${encodeURIComponent(pageTitle)}&pithumbsize=200&origin=*`
+            )
+            const imageData = await imageResponse.json()
+            
+            const pages = imageData.query.pages
+            const pageId = Object.keys(pages)[0]
+            
+            return {
+              name: actorName,
+              image: pages[pageId].thumbnail ? pages[pageId].thumbnail.source : null
+            }
+          }
+        } catch (err) {
+          console.error(`Error fetching image for ${actorName}:`, err.message)
+          return {
+            name: actorName,
+            image: null
+          }
+        }
+        
+        return {
+          name: actorName,
+          image: null
+        }
+      })
+      
+      // Wait for all actor image fetches to complete
+      const actorsWithImages = await Promise.all(actorImagePromises)
+      
+      // Render the view with movie data and actors with images
+      res.render('./movies/show.ejs', { 
+        movie, 
+        actors: actorsWithImages,
+        user: req.session.user 
+      })
     } else {
       res.status(404).send('Movie not found.')
     }
