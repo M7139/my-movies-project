9560f25498d1ad094b67f9b95010e40741e3c2f3
Manaf-10 <manaf.alhujairi2006@gmail.com>
2025-07-25 21:07:21 +0300

    fixed the change pass and sugn-up issues

--- routes/authRouter.js
+++ routes/authRouter.js
@@ -1,14 +1,18 @@
 const express = require('express')
+const upload = require('../uploads/upload.js')
+
 const router = express.Router()
 const authController = require('../controllers/authController.js')
 const { isLoggedIn } = require('../middlewares/auth.js')
 
 router.get('/sign-up', authController.auth_sign_up_get)
-router.post('/sign-up', authController.auth_sign_up_post)
+router.post('/sign-up', upload.single('picture'), authController.auth_sign_up_post)
 
 router.get('/sign-in', authController.auth_sign_in_get)
 router.post('/sign-in', authController.auth_sign_in_post)
 
 router.get('/sign-out', isLoggedIn, authController.auth_sign_out_post)
+router.put('/:id', authController.auth_update_password_put)
 
 module.exports = router
+
